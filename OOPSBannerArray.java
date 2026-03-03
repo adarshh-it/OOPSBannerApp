72ddc885532eb0402e0ba33d416ff8b04e3a9787
adarshh-it <adarsh022005@gmail.com>
2026-03-03 11:29:28 +0530

    Merged feature/UC3-StringJoin into dev

---
+++ OOPSBannerArray.java
@@ -0,0 +1,25 @@
+public class OOPSBannerArray {
+
+    public static void main(String[] args) {
+        printBanner();
+    }
+
+    public static void printBanner() {
+
+        String[] banner = {
+
+                String.join("   ", " ***** ", " ***** ", " ***** ", " ***** "),
+                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+                String.join("   ", "*     *", "*     *", "*     *", "*     *"),
+                String.join("   ", " ***** ", " ***** ", " ***** ", " ***** ")
+
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
